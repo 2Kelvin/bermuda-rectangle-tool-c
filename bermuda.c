15b2abcf6f5b0f01d3f38be6b5db9f5dd72cfbb3
2Kelvin <rocketmancodes02@gmail.com>
2023-04-04 10:43:47 +0300

    Pass arguments to main

--- bermuda.c
+++ bermuda.c
@@ -7,20 +7,29 @@
  * Return: 0 (Always for Success)
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
     char line[80];
 
+    /* argc is the number of array items in argv */
+    if (argc != 6)
+    {
+        fprintf(stderr, "You need to give 5 arguments\n");
+        return (1);
+    }
+
+    /* array argv contains array search arguments and files */
+    /* this array arguments are accessed using array destructuring e.g. argv[2] */
     FILE *in = fopen("spooky.csv", "r");
-    FILE *file1 = fopen("ufos.csv", "w");
-    FILE *file2 = fopen("disappearances.csv", "w");
-    FILE *file3 = fopen("others.csv", "w");
+    FILE *file1 = fopen(argv[2], "w");
+    FILE *file2 = fopen(argv[4], "w");
+    FILE *file3 = fopen(argv[5], "w");
 
     while (fscanf(in, "%79[^\n]\n", line) == 1)
     {
-        if (strstr(line, "UFO"))
+        if (strstr(line, argv[1]))
             fprintf(file1, "%s\n", line);
-        else if (strstr(line, "Disappearance"))
+        else if (strstr(line, argv[3]))
             fprintf(file2, "%s\n", line);
         else
             fprintf(file3, "%s\n", line);
@@ -33,4 +42,4 @@ int main(void)
     return (0);
 }
 
-// RUN THE PROGRAM SEE WHAT IT DOES DAWG!!
+/* run the program using ==> ./bermuda.exe UFO aliens.csv Elvis elvises.csv the_rest.csv*/
